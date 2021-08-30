b2175646a0d45e52eb28230a03e26f54a9aab037
gvnfcosta <Astor@000>
2021-08-30 00:51:02 -0300

    Aula de Introdução Orientada a Objetos
    Recebe nome do aluno, e as notas dos 3 primeiros trimestres, impede valor superior, calcula a média e informa o resultado, e quanto faltou.

--- Fila/src/main/java/udemy/estudante/Estudante.java
+++ Fila/src/main/java/udemy/estudante/Estudante.java
@@ -14,35 +14,14 @@ public class Estudante {
 
         Notas notas = new Notas();
 
-
         System.out.println("Entre com os dados");
         System.out.print("Nome: ");
         notas.nome = sc.nextLine();
         System.out.println(notas.nome);
 
-        for(int i=0; i<3; ++i){
-            System.out.print("Entre com a nota do " + (i + 1) + " trimestre: ");
-            boolean j = true;
+        notas.entraNotas();
 
-                while (j) {
-                    notas.nota[i] = sc.nextDouble();
-                    if (i == 0 && notas.nota[i] > 30){
-                        System.out.print("A nota não deve ser maior que 30, entre novamente: ");
-                    }else if (i == 0){
-                        j = false;
-                    }
-
-                    if (i > 0 && notas.nota[i] > 35) {
-                        System.out.print("A nota não deve ser maior que 35, entre novamente: ");
-                    } else if (i > 0) {
-                        j = false;
-                    }
-                }
-        }
-
-        for (int i=0; i<3; ++i){
-            System.out.println("Nota do " + i + " trimestre: " + notas.nota[i]);
-        }
+        notas.exibeNotas();
 
         System.out.println("Nota final: " + notas.calculaNotaFinal());
         System.out.println("Aluno " + notas.nome + " foi " + notas.calculaNotaFinal());
--- Fila/src/main/java/udemy/estudante/Notas.java
+++ Fila/src/main/java/udemy/estudante/Notas.java
@@ -1,12 +1,44 @@
 package udemy.estudante;
 
+import java.util.Scanner;
+
 public class Notas {
 
     public String nome;
     public double nota[] = {0, 0, 0};
 
+    Scanner sc = new Scanner(System.in);
+
     public double calculaNotaFinal() {
         return nota[0] + nota[1] + nota[2];
+    }
+
+    public void entraNotas(){
+        for(int i=0; i<3; ++i){
+            System.out.print("Entre com a nota do " + (i + 1) + " trimestre: ");
+            boolean j = true;
+
+            while (j) {
+                nota[i] = sc.nextDouble();
+                if (i == 0 && nota[i] > 30){
+                    System.out.print("A nota não deve ser maior que 30, entre novamente: ");
+                }else if (i == 0){
+                    j = false;
+                }
+
+                if (i > 0 && nota[i] > 35) {
+                    System.out.print("A nota não deve ser maior que 35, entre novamente: ");
+                } else if (i > 0) {
+                    j = false;
+                }
+            }
+        }
+    }
+
+    public void exibeNotas() {
+        for (int i = 0; i < 3; ++i) {
+            System.out.println("Nota do " + i + " trimestre: " + nota[i]);
+        }
     }
 
     public double pontosFaltando(){
